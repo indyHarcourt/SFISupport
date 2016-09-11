0656cc24b5a89a19f230ad5f3bce5d13e999036c
John Harcourt <john.harcourt@inin.com>
2016-09-11 19:06:39 -0400

    Init

---
+++ Default.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Debug="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script src="https://login.salesforce.com/canvas/sdk/js/37.0/canvas-all.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jsforce/1.7.0/jsforce.min.js"></script>
+
+    <title>Canvas App</title>
+
+    <script>
+        $(document).ready(function(){
+            $('#supportTable tr:nth-child(odd)').addClass('odd');
+        });
+    </script>
+
+    <style>
+        #supportTable, th, td {
+            border: 1px solid black;
+        }
+
+        #supportTable {
+            border-collapse:collapse;
+        }
+
+        .odd{
+            background-color: whitesmoke;
+        }
+            
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .request {
+            font-family: Courier New, Courier, monospace;
+            display:none;
+            width: 100%;
+        }
+
+        #userTable {
+            width: 100%;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+            #userTable th {
+                text-align: left;
+            }
+    </style>
+</head>
+
+
+
+<body>
+    <form id="form1" runat="server">
+        <div style="width: 100%; padding: 5px; background-color: aliceblue;">
+            <table id="userTable" style="width:1000px;">
+                <tr>
+                    <th style="width: 150px;">User:</th>
+                    <td>
+                        <%=UserName %>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Account:</th>
+                    <td>
+                        <%=accountName %>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <h3>ISupport Tickets</h3>
+            <table id="supportTable" style="width:1000px;">
+                <thead>
+                    <tr>
+                        <th>Number</th>
+                        <th>Name</th>
+                        <th>Created Date</th>
+                        <th>Status</th>
+                        <th>State</th>
+                        <th>Last Updated</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>12345</td>
+                    <td>Ticket 12345</td>
+                    <td>7/12/2016</td>
+                    <td>Open</td>
+                    <td>State?</td>
+                    <td>8/10/2016</td>
+                </tr>
+                <tr>
+                    <td>12366</td>
+                    <td>Ticket 12366</td>
+                    <td>7/19/2016</td>
+                    <td>Open</td>
+                    <td>State?</td>
+                    <td>8/19/2016</td>
+                </tr>
+            </table>
+
+        </div>
+
+
+        <div class="request">
+            <%=SignedRequestStatus%>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+using SalesForceOAuth;
+using Newtonsoft.Json.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string SignedRequestStatus;
+    public string UserName = string.Empty;
+    public string accountId = string.Empty;
+    public string accountName = String.Empty;
+
+    private RootObject root;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string signedRequest = Request.Params["signed_request"];
+        SignedRequestStatus = CheckSignedRequest(Request.Form["signed_request"]);
+        if (root == null)
+        {
+            UserName = "root is null";
+        }
+        else
+        {
+            UserName = root.context.user.fullName;
+        }
+        
+        if (root.context.environment.parameters.ContainsKey("acctId"))
+        {
+            accountId = root.context.environment.parameters["acctId"];
+        }
+        if (root.context.environment.parameters.ContainsKey("acctName"))
+        {
+            accountName = root.context.environment.parameters["acctName"];
+        }
+        
+        
+    }
+    private string CheckSignedRequest(string encodedSignedRequest)
+    {
+        if (String.IsNullOrEmpty(encodedSignedRequest))
+        {
+            // Failed because we are not in canvas, so exit early
+            return "Did not find 'signed_request' POSTed in the HttpRequest. Either we are not being called by a SalesForce Canvas, or its associated Connected App isn't configured properly.";
+        }
+
+        // Validate the signed request using the consumer secret
+        string secret = GetConsumerSecret();
+        var auth = new SalesForceOAuth.SignedAuthentication(secret, encodedSignedRequest);
+        if (!auth.IsAuthenticatedCanvasUser)
+        {
+            // failed because the request is either a forgery or the connected app doesn't match our consumer secret
+            return "SECURITY ALERT: We received a signed request, but it did not match our consumer secret. We should treat this as a forgery and stop processing the request.";
+        }
+      
+        root = auth.CanvasContextObject;
+        return String.Format("SUCCESS! Here is the signed request decoded as JSON:\n{0}", auth.CanvasContextJson);
+    }
+
+    private string GetConsumerSecret()
+    {
+        // Since the consumer secret shouldn't change often, we'll put it in the Application Cache. You may want cache it differently in a production application.
+        string cachedConsumerSecret = (HttpContext.Current.Application["ConsumerSecret"] ?? String.Empty).ToString();
+        if (!String.IsNullOrEmpty(cachedConsumerSecret))
+        {
+            return cachedConsumerSecret;
+        }
+
+        // We use key names in the format "cs-key:<server>:<port><app-path>" so that we 
+        // can maintain a consumer secret per server + port + app instance
+        //string key = String.Format("cs-key:{0}:{1}{2}",
+        //    Request.ServerVariables["SERVER_NAME"],
+        //    Request.Url.Port,
+        //    Request.ApplicationPath);
+        string key = "cs-key";
+        string secret = ConfigurationManager.AppSettings[key];
+        if (!String.IsNullOrEmpty(secret))
+        {
+            HttpContext.Current.Application["ConsumerSecret"] = secret;
+        }
+        return secret;
+    }
+}
