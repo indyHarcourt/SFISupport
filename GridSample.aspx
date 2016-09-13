d55a5ad7d673ad161242e5137b8a4ecad022a1bd
John Harcourt <john.harcourt@inin.com>
2016-09-13 09:36:45 -0400

    added grid sample

--- GridSample.aspx
+++ GridSample.aspx
@@ -1,71 +1,6 @@
-﻿<%@ Page Language="C#" AutoEventWireup="True" %>
+﻿<%@ Page Language="C#" AutoEventWireup="True" CodeFile="GridSample.aspx.cs" Inherits="GridSample" %>
 
-<%@ Import Namespace="System.Data" %>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<script language="C#" runat="server">
-
-    string SortExpression;
-        public DataView dv;
-        DataTable dt = new DataTable();
-    ICollection CreateDataSource()
-    {
-        if (dv == null)
-        {            
-            Random r = new Random();
-
-            DataTable dt = new DataTable();
-            DataRow dr;
-
-            dt.Columns.Add(new DataColumn("IntegerValue", typeof(Int32)));
-            dt.Columns.Add(new DataColumn("StringValue", typeof(string)));
-            dt.Columns.Add(new DataColumn("CurrencyValue", typeof(double)));
-
-            for (int i = 0; i < 20; i++)
-            {
-                dr = dt.NewRow();
-
-                dr[0] = i;
-                dr[1] = "Item " + r.Next(100);
-                dr[2] = 1.23 * (r.Next(50));
-
-                dt.Rows.Add(dr);
-            }
-
-            dv = new DataView(dt);
-
-        }
-
-        dv.Sort=SortExpression;
-        return dv;
-    }
-
-    void Page_Load(Object sender, EventArgs e)
-    {
-        if (SortExpression == string.Empty)
-        {
-            SortExpression = "IntegerValue";
-        }
-        if (!IsPostBack)
-        {
-            // Load this data only once.
-
-            ItemsGrid.DataSource = CreateDataSource();
-            ItemsGrid.DataBind();
-        }
-    }
-
-    void Sort_Grid(Object sender, DataGridSortCommandEventArgs e)
-    {
-        SortExpression = e.SortExpression.ToString();
-        ItemsGrid.DataSource = CreateDataSource();
-        ItemsGrid.DataBind();
-    }
-
-</script>
-
+<html>
 <head runat="server">
     <title>DataGrid Example</title>
 </head>
@@ -77,18 +12,11 @@
 
         <b>Product List</b>
 
-        <asp:DataGrid ID="ItemsGrid"
-            BorderColor="black"
-            BorderWidth="1"
-            AllowSorting="true"
-            CellPadding="3"
-            OnSortCommand = "Sort_Grid"
-            AutoGenerateColumns="true"
-            runat="server">
+    <p>
+        <asp:GridView ID="GridView1" runat="server" AllowSorting="true" OnSorting="GridView1_Sorting">
+        </asp:GridView>
+    </p>
 
-            <HeaderStyle BackColor="#00aaaa"></HeaderStyle>
-
-        </asp:DataGrid>
 
     </form>
 
--- GridSample.aspx.cs
+++ GridSample.aspx.cs
@@ -1,14 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
-public partial class GridSample : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class GridSample : System.Web.UI.Page
     {
+        private IList<Student> _students;
+        private Student _student;
+        private DataSet ds;
+        private DataTable dataTable;
+        Image sortImage = new Image();
+        public string SortDireaction
+        {
+            get
+            {
+                if (ViewState["SortDireaction"] == null)
+                    return string.Empty;
+                else
+                    return ViewState["SortDireaction"].ToString();
+            }
+            set
+            {
+                ViewState["SortDireaction"] = value;
+            }
+        }
+        private string _sortDirection;
 
+
+        public GridSample()
+        {
+            //Create Student List 
+            _students = new List<Student>();
+            _student = new Student();
+            _student.RollNumber = 11;
+            _student.Class = "5Th";
+            _student.Div = "A";
+            _student.Name = "Raju";
+            _students.Add(_student);
+            _student = new Student();
+            _student.RollNumber = 13;
+            _student.Class = "6Th";
+            _student.Div = "A1";
+            _student.Name = "Chanki";
+            _students.Add(_student);
+            _student = new Student();
+            _student.RollNumber = 21;
+            _student.Class = "5Th";
+            _student.Div = "A3";
+            _student.Name = "Amit";
+            _students.Add(_student);
+            _student = new Student();
+            _student.RollNumber = 61;
+            _student.Class = "9Th";
+            _student.Div = "A4";
+            _student.Name = "Suresh";
+            _students.Add(_student);
+
+            dataTable = new DataTable("Student");
+            //Add column
+            // Add columns.
+            for (int i = 0; i < 4; i++)
+            {
+                dataTable.Columns.Add();
+            }
+            // Add rows.
+            foreach (var item in _students)
+            {
+                string[] obj = new string[4];
+                obj[0] = item.Name;
+                obj[1] = item.Class;
+                obj[2] = item.Div;
+                obj[3] = item.RollNumber.ToString();
+                dataTable.Rows.Add(obj);
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+
+        protected void BindGrid()
+        {
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            SetSortDirection(SortDireaction);
+            if (dataTable != null)
+            {
+                //Sort the data.
+                dataTable.DefaultView.Sort = e.SortExpression + " " + _sortDirection;
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+                SortDireaction = _sortDirection;
+                int columnIndex = 0;
+                foreach (DataControlFieldHeaderCell headerCell in GridView1.HeaderRow.Cells)
+                {
+                    if (headerCell.ContainingField.SortExpression == e.SortExpression)
+                    {
+                        columnIndex = GridView1.HeaderRow.Cells.GetCellIndex(headerCell);
+                    }
+                }
+
+                GridView1.HeaderRow.Cells[columnIndex].Controls.Add(sortImage);
+            }
+        }
+
+        protected void SetSortDirection(string sortDirection)
+        {
+            if (sortDirection == "ASC")
+            {
+                _sortDirection = "DESC";
+                sortImage.ImageUrl = "view_sort_ascending.png";
+
+            }
+            else
+            {
+                _sortDirection = "ASC";
+                sortImage.ImageUrl = "view_sort_descending.png";
+            }
+        }
     }
-}
+
+    public class Student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Div { get; set; }
+    }
+
